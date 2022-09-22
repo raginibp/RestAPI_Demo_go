2a525d043eb2efb9c38085bfc27101fc9c77a265
raginibp <raginipatel155@gmail.com>
2022-09-22 14:50:37 +0530

    fixed

--- controllers/albums.go
+++ controllers/albums.go
@@ -19,12 +19,6 @@ type album struct {
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
-//var albums = []album{
-//	{ID: "1", Title: "Blue Train", Artist: "John Coltrane", Price: 56.99},
-//	{ID: "2", Title: "Jeru", Artist: "Gerry Mulligan", Price: 17.99},
-//	{ID: "3", Title: "Sarah Vaughan and Clifford Brown", Artist: "Sarah Vaughan", Price: 39.99},
-//}
-
 func CreateAlbumTable(db *pg.DB) error {
 	opts := &orm.CreateTableOptions{
 		IfNotExists: true,

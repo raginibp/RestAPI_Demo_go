96c7fcc8ed34c357284254e88cfcb12e1e1eba20
raginibp <raginipatel155@gmail.com>
2022-09-22 17:29:28 +0530

    Welcome test

---
+++ test/albums_test.go
@@ -0,0 +1,30 @@
+package t_test
+
+import (
+	"RestAPI_Demo/controllers"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/assert/v2"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+	return router
+
+}
+func TestWelcome(t *testing.T) {
+	mockResponse := `{"message":"Welcome to Go Rest Api","status":200}`
+
+	r := SetUpRouter()
+	r.GET("/", controllers.Welcome)
+	req, _ := http.NewRequest("GET", "/", nil)
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+
+	responseData, _ := ioutil.ReadAll(w.Body)
+	assert.Equal(t, mockResponse, string(responseData))
+	assert.Equal(t, http.StatusOK, w.Code)
+}
